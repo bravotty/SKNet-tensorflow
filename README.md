13fbcec32df860b6b612035dd2a1d959afdcb1bb
bravotty <tychen@m.scnu.edu.cn>
2019-04-16 20:38:54 +0800

    update SKNet block code and readme

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # SKNet-tensorflow
-Simple tensorflow implementation of "Selective Kernel Networks"
+Simple tensorflow implementation of [Selective Kernel Networks](<https://arxiv.org/pdf/1903.06586.pdf>)
+
+If you want to see the ***original author's code***, pls refer to this github [link](<https://github.com/implus/SKNet>)
+
+***Version 1.0*** : SKNet block without groups and BN params - ***coming soon***.
+
+
+
+## Usage : 
+
+```
+import  SKNet
+import tensorflow.contrib.slim as slim
+
+...
+conv1 = slim.conv2d(inputs, 64, [3, 3], scope='conv1')
+conv2 = SKNet(conv1, 3, 2)
+conv3 = slim.conv2d(inputs, 3, [3, 3],  scope='out')
+...
+
+training code
+```
+
---
+++ SKNet.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+@Author : tyty
+@Date   : 2018.4.16
+@E-mail : bravotty@protonmail.com
+"""
+import tensorflow as tf
+import tflearn
+import tensorflow.contrib.slim as slim
+
+def SKConv(input, M, r, L=32, stride=1, is_training=True):
+    input_feature = input.get_shape().as_list()[3]
+    d = max(int(input_feature / r), L)
+    net = input
+    with slim.arg_scope([slim.conv2d, slim.fully_connected],
+                        activation_fn=tf.nn.relu,
+                        normalizer_fn=slim.batch_norm,
+                        normalizer_params={'is_training': is_training, 'decay': 0.95}):
+        for i in range(M):
+            net = slim.conv2d(net, input_feature, [3+i*2, 3+i*2], rate=1+i, stride=stride)
+            net = tf.nn.relu(net)
+            if i == 0:
+                fea_U = net
+            else:
+                fea_U = tf.add(fea_U, net)
+        gap = tflearn.global_avg_pool(net)
+        fc  = slim.fully_connected(gap, d, activation_fn=None)
+        fcs = fc
+        for _ in range(M):
+            fcs = slim.fully_connected(fcs, input_feature, activation_fn=None)
+            if _ == 0:
+                att_vec = fcs
+            else:
+                att_vec = tf.add(att_vec, fcs)
+        att_vec = tf.expand_dims(att_vec, axis=1)
+        att_vec = tf.expand_dims(att_vec, axis=1)
+        att_vec_softmax = tf.nn.softmax(att_vec)
+        fea_v = tf.multiply(fea_U, att_vec_softmax)
+    return fea_v
